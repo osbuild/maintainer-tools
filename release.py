f529c0428d34725df51cee40d77cc12cc4148f90
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2021-08-04 17:41:27 +0200

    release-helper: Also match ssh for git remotes
    Also restructure the code a bit so we can show the default values
    already in the argparser help output.

--- release.py
+++ release.py
@@ -7,7 +7,8 @@
 import argparse
 import subprocess, sys
 import os
-
+import getpass
+from re import search
 
 # Check if we are in a git repo, on the right branch and up-to-date
 def sanity_checks():
@@ -20,8 +21,6 @@ def sanity_checks():
     if current_branch != "main":
         print(f"Error: You are not on the 'main' branch but on branch '{current_branch}'.")
         sys.exit(1)
-    else:
-        print(f"Updating '{current_branch}' to avoid conflicts...")
 
     is_clean = run_command(['git', 'status', '--untracked-files=no', '--porcelain'])
     if is_clean != "":
@@ -29,8 +28,6 @@ def sanity_checks():
         print(f"The working directory is not clean.\n"
                "You have the following unstaged or uncommitted changes:\n"
                "{status}")
-    else:
-        run_command(['git', 'pull'])
 
 # Run a shellcommand and return stdout
 def run_command(argv):
@@ -55,7 +52,7 @@ def autoincrement_version():
 
 # Guess the git remote to push the release changes to
 def guess_remote(repo):
-    origin = f"github.com/osbuild/{repo}.git"
+    origin = f"github.com[/:]osbuild/{repo}.git"
     remotes = run_command(['git','remote']).split("\n")
     if len(remotes) > 2:
         print(f"You have more than two 'git remotes' specified, so guessing the correct one will most likely fail.\n"
@@ -64,47 +61,45 @@ def guess_remote(repo):
 
     for remote in remotes:
         remote_url = run_command(['git','remote','get-url',f'{remote}'])
-        if remote_url.__contains__(origin) == False:
+        if search(origin, remote_url) is not None:
             return remote
 
 # Execute all steps of the release playbook
-def release_playbook(version, remote, repo):
-    step(f"Check out a new branch for the release {version}", ['git', 'checkout', '-b', f'release-{version}'])
+def release_playbook(args, repo):
+    step(f"Check out a new branch for the release {args.version}", ['git', 'checkout', '-b', f'release-{args.version}'])
     step("Generate template for new release", ['make', 'release'])
-    step(f"Bump the version to {version}", ['make', 'bump-version'])
-    step(f"Please make sure the version was bumped correctly to {version}", ['git', 'diff'])
+    step(f"Bump the version to {args.version}", ['make', 'bump-version'])
+    step(f"Please make sure the version was bumped correctly to {args.version}", ['git', 'diff'])
     # TODO: Call the pr_summaries.py script for osbuild or assemble the news from docs/news/unreleased for composer
     step(f"Add and commit the release-relevant changes ({repo}.spec NEWS.md setup.py)",
-          ['git', 'commit', f'{repo}.spec', 'NEWS.md', 'setup.py', '-s', f'-m {version}', f'-m "Release osbuild {version}"'])
-    step(f"Push all release changes to the remote '{remote}'",
-          ['git', 'push', '--set-upstream', f'{remote}', f'release-{version}'])
+          ['git', 'commit', f'{repo}.spec', 'NEWS.md', 'setup.py', '-s', f'-m {args.version}', f'-m "Release osbuild {args.version}"'])
+    step(f"Push all release changes to the remote '{args.remote}'",
+          ['git', 'push', '--set-upstream', f'{args.remote}', f'release-{args.version}'])
     # TODO: Create a PR on GitHub automatically (since we know all the necessary infos) and paste a link to it in stdout
     print("Please use github to submit a pull-request against the main repository!")
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--version", help="Set the `version` for the release")
-    parser.add_argument("--remote", help="Set the `remote` on github to push the release changes to")
-    args = parser.parse_args()
-
     # Do some initial sanity checking of the repository and its state
     sanity_checks()
 
-    # Determine the version for the release
-    if (args.version is None):
-        version = autoincrement_version()
-    else:
-        version = args.version
-    
-    # Determine the remote to push the release to
+    # Get some basic fallback/default values
     repo = os.path.basename(os.getcwd())
-    if (args.remote is None):
-        remote = guess_remote(repo)
-    else:
-        remote = args.remote
+    version = autoincrement_version()
+    remote = guess_remote(repo)
+    username = getpass.getuser()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", help=f"Set the version for the release (Default: {version})", default=version)
+    parser.add_argument("--remote", help=f"Set the git remote on github to push the release changes to (Default: {remote})", default=remote)
+    parser.add_argument("--user", help=f"Set the username on github (Default: {username})", default=username)
+    parser.add_argument("--token", help="Set the github token used to authenticate")
+    args = parser.parse_args()
+
+    print(f"Updating branch 'main' to avoid conflicts...")
+    run_command(['git', 'pull'])
 
     # Run the release playbook
-    release_playbook(version, remote, repo)
+    release_playbook(args, repo)
 
 
 if __name__ == "__main__":
