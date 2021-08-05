213ddc3304aeaa6e605132cbb98266ee9d39a45f
Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>
2021-08-05 22:29:16 +0200

    release-helper: Satisfy pylint and PEP8

--- release.py
+++ release.py
@@ -5,34 +5,39 @@
 #
 
 import argparse
-import subprocess, sys
+import subprocess
+import sys
 import os
 import getpass
 from re import search
 import requests
 
-# FIXME: Somehow these colors don't do what they should yet...
+
 class fg:
-    BOLD = '\033[1m' # bold
-    OK = '\033[32m' # green
-    WARNING = '\033[33m' # yellow
-    ERROR = '\033[31m' # red
-    RESET = '\033[0m' # reset
+    BOLD = '\033[1m'  # bold
+    OK = '\033[32m'  # green
+    WARNING = '\033[33m'  # yellow
+    ERROR = '\033[31m'  # red
+    RESET = '\033[0m'  # reset
+
 
 def msg_error(body):
     print(f"{fg.ERROR}{fg.BOLD}Error:{fg.RESET} {body}")
     sys.exit(1)
 
+
 def msg_info(body):
     print(f"{fg.WARNING}{fg.BOLD}Info:{fg.RESET} {body}")
+
 
 def msg_ok(body):
     print(f"{fg.OK}{fg.BOLD}OK:{fg.RESET} {body}")
 
-# Check if we are in a git repo, on the right branch and up-to-date
+
 def sanity_checks():
+    """Check if we are in a git repo, on the right branch and up-to-date"""
     is_git = run_command(['git', 'rev-parse', '--is-inside-work-tree'])
-    if  is_git != "true":
+    if is_git != "true":
         msg_error("This is not a git repository.")
 
     current_branch = run_command(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
@@ -49,18 +54,25 @@ def sanity_checks():
                  f"{status}")
     return current_branch
 
-# Run a shellcommand and return stdout
+
 def run_command(argv):
-    result = subprocess.run(argv, capture_output=True, text=True, encoding='utf-8').stdout
+    """Run a shellcommand and return stdout"""
+    result = subprocess.run(
+        argv,
+        capture_output=True,
+        text=True,
+        check=True,
+        encoding='utf-8').stdout
     return result.strip()
 
-# Ask the user for confirmation on whether to accept (y) or skip (s) the step or cancel (N) the playbook
+
 def step(action, args):
+    """Ask the user for confirmation on whether to accept (y) or skip (s) the step or cancel (N) the playbook"""
     feedback = input(f"{fg.BOLD}Step: {fg.RESET}{action} [y/s/N] ")
     if feedback == "y":
         if args is not None:
             out = run_command(args)
-            msg_ok (f"\n{out}")
+            msg_ok(f"\n{out}")
     elif feedback == "s":
         msg_info("Step skipped.")
         return
@@ -68,80 +80,91 @@ def step(action, args):
         msg_info("Release playbook canceled.")
         sys.exit(0)
 
-# Bump the version of the latest git tag by 1
+
 def autoincrement_version():
+    """Bump the version of the latest git tag by 1"""
     latest_tag = run_command(['git', 'describe', '--abbrev=0'])
-    version = int(latest_tag.replace("v","")) + 1
+    version = int(latest_tag.replace("v", "")) + 1
     return version
 
-# Guess the git remote to push the release changes to
+
 def guess_remote(repo):
+    """Guess the git remote to push the release changes to"""
     origin = f"github.com[/:]osbuild/{repo}.git"
-    remotes = run_command(['git','remote']).split("\n")
+    remotes = run_command(['git', 'remote']).split("\n")
     if len(remotes) > 2:
         msg_info("You have more than two 'git remotes' specified, so guessing the correct one will most likely fail.\n"
                  "Please use the --remote argument to set the correct one.\n"
                  f"{remotes}")
 
     for remote in remotes:
-        remote_url = run_command(['git','remote','get-url',f'{remote}'])
+        remote_url = run_command(['git', 'remote', 'get-url', f'{remote}'])
         if search(origin, remote_url) is None:
             return remote
+    return None
 
-# Update the NEWS file for osbuild
-# FIXME: Need to decide how to handle the pr_summaries.py file (make it available as .egg?) so it can be properly invoked
+
 def update_news_osbuild(args):
-    step(f"Collect all PRs for milestone {args.version}", ['../maintainer-tools/pr_summaries.py','--version',f'{args.version}','--token',f'{args.token}'])
+    """Update the NEWS file for osbuild"""
+    # FIXME: Need to decide how to handle the pr_summaries.py file (make it available as .egg?)
+    # so it can be properly invoked
+    step(f"Collect all PRs for milestone {args.version}", [
+         '../maintainer-tools/pr_summaries.py', '--version', f'{args.version}', '--token', f'{args.token}'])
     if os.path.exists(f'NEWS-{args.version}.md'):
         msg_ok(f"NEWS file generated: {run_command(['ls','-l',f'NEWS-{args.version}.md'])}")
-        step(f"Edit the NEWS-{args.version}.md file", [f'{args.editor}',f'NEWS-{args.version}.md'])
+        step(f"Edit the NEWS-{args.version}.md file", [f'{args.editor}', f'NEWS-{args.version}.md'])
     else:
         msg_info("Something went wrong collecting the pull requests (or you skipped the step).")
 
-# Update the NEWS file for osbuild-composer
-def update_news_composer():
+
+def update_news_composer(args):
+    """Update the NEWS file for osbuild-composer"""
     step("Create a docs directory for this release and move all news files to it",
-         ['mkdir',f'docs/news/{args.version}','&&','mv','docs/news/unreleased/*',f'docs/news/{args.version}'])
+         ['mkdir', f'docs/news/{args.version}', '&&', 'mv', 'docs/news/unreleased/*', f'docs/news/{args.version}'])
     msg_info(f"Content of docs/news/{args.version}:\n{run_command(['ls',f'docs/news/{args.version}'])}")
     step("Generate template for new release", ['make', 'release'])
-    step("Edit the NEWS.md file", [f'{args.editor}','NEWS.md'])
+    step("Edit the NEWS.md file", [f'{args.editor}', 'NEWS.md'])
 
-# Bump the version in the spec file
+
 def bump_version(version, filename):
+    """Bump the version in a file"""
     latest_tag = run_command(['git', 'describe', '--abbrev=0'])
     with open(filename, 'r') as file:
         content = file.read()
 
     # Maybe use re.sub in case the version appears a second time in the spec file
-    content = content.replace(latest_tag.replace("v",""), str(version))
+    content = content.replace(latest_tag.replace("v", ""), str(version))
 
     with open(filename, 'w') as file:
         file.write(content)
 
-# Create a pull request on GitHub from the fork to the main repository
+
 def create_pullrequest(args, repo):
+    """Create a pull request on GitHub from the fork to the main repository"""
     if args.user is None or args.token is None:
         msg_error("Missing credentials for GitHub.")
 
     msg_info(f"Creating a pull request on github for user {args.user}")
     url = f'https://api.github.com/repos/osbuild/{repo}/pulls'
-    payload = {'head':f'{args.user}:release-{args.version}',
-               'base':'main',
-               'title':f'Prepare release {args.version}',
-               'body':'Tasks:\n- Bump version\n-Update news',
-              }
+    payload = {'head': f'{args.user}:release-{args.version}',
+               'base': 'main',
+               'title': f'Prepare release {args.version}',
+               'body': 'Tasks:\n- Bump version\n-Update news',
+               }
 
-    r = requests.post(url, json = payload, auth=(args.user,args.token))
+    r = requests.post(url, json=payload, auth=(args.user, args.token))
     # FIXME: Check the return code (201 for success) properly
     try:
         msg_ok(f"Pull request successfully created: {r.json()['url']}")
     except:
         msg_error(f"Failed to create pull request. {r.json()}")
 
-# Execute all steps of the release playbook
+
 def release_playbook(args, repo, current_branch):
-    if current_branch.__contains__("release") == False:
-        step(f"Check out a new branch for the release {args.version}", ['git', 'checkout', '-b', f'release-{args.version}'])
+    """Execute all steps of the release playbook"""
+    if "release" not in current_branch:
+        step(f"Check out a new branch for the release {args.version}", [
+             'git', 'checkout', '-b', f'release-{args.version}'])
 
     if repo == "osbuild":
         update_news_osbuild(args)
@@ -151,21 +174,25 @@ def release_playbook(args, repo, current_branch):
     step(f"Bump the version in the {repo}.spec", None)
     bump_version(args.version, f"{repo}.spec")
     if repo == "osbuild":
-        bump_version(args.version,"setup.py")
+        bump_version(args.version, "setup.py")
 
     print(f"{run_command(['git', 'diff'])}")
     step(f"Please make sure the version was bumped correctly to {args.version}", None)
 
     if repo == "osbuild":
         step(f"Add and commit the release-relevant changes ({repo}.spec NEWS.md setup.py)",
-            ['git', 'commit', f'{repo}.spec', 'NEWS.md', 'setup.py', '-s', f'-m {args.version}', f'-m "Release osbuild {args.version}"'])
+             ['git', 'commit', f'{repo}.spec', 'NEWS.md', 'setup.py',
+              '-s', f'-m {args.version}', f'-m "Release osbuild {args.version}"'])
     elif repo == "osbuild-composer":
         step(f"Add and commit the release-relevant changes ({repo}.spec NEWS.md setup.py)",
-            ['git','add','docs/news','&&','git', 'commit', f'{repo}.spec', 'NEWS.md', 'docs/news/unreleased',f'docs/news/{args.version}', '-s', f'-m {args.version}', f'-m "Release osbuild {args.version}"'])
+             ['git', 'add', 'docs/news', '&&', 'git', 'commit', f'{repo}.spec', 'NEWS.md',
+              'docs/news/unreleased', f'docs/news/{args.version}', '-s',
+              f'-m {args.version}', f'-m "Release osbuild {args.version}"'])
 
     step(f"Push all release changes to the remote '{args.remote}'",
-          ['git', 'push', '--set-upstream', f'{args.remote}', f'release-{args.version}'])
+         ['git', 'push', '--set-upstream', f'{args.remote}', f'release-{args.version}'])
     create_pullrequest(args, repo)
+
 
 def main():
     # Do some initial sanity checking of the repository and its state
@@ -181,10 +208,14 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--version", help=f"Set the version for the release (Default: {version})", default=version)
-    parser.add_argument("--remote", help=f"Set the git remote on github to push the release changes to (Default: {remote})", default=remote)
+    parser.add_argument(
+        "--remote", help=f"Set the git remote on github to push the release changes to (Default: {remote})",
+        default=remote)
     parser.add_argument("--user", help=f"Set the username on github (Default: {username})", default=username)
     parser.add_argument("--token", help="Set the github token used to authenticate")
-    parser.add_argument("--editor", help=f"Set which editor shall be used for editing text (e.g. NEWS) files (Default: {editor}", default=editor)
+    parser.add_argument(
+        "--editor", help=f"Set which editor shall be used for editing text (e.g. NEWS) files (Default: {editor}",
+        default=editor)
     args = parser.parse_args()
 
     msg_info(f"Updating branch '{current_branch}' to avoid conflicts...\n{run_command(['git', 'pull'])}")
