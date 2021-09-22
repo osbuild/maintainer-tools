4699a3d27c8082b6a46a42cc49a8cc7af8b834f1
Simon Steinbeiss <simon.steinbeiss@redhat.com>
2021-09-22 17:38:19 +0200

    release-helper: Drop skip-ci option again
    Otherwise we don't get RPMs in S3

--- release.py
+++ release.py
@@ -406,7 +406,7 @@ def release_playbook(args, repo, api):
     if repo == "osbuild":
         step(f"Add and commit the release-relevant changes ({repo}.spec NEWS.md setup.py)",
              ['git', 'commit', f'{repo}.spec', 'NEWS.md', 'setup.py',
-              '-s', '-m', f'{args.version}', '-m', f'Release osbuild {args.version}','-m','[skip ci]'],
+              '-s', '-m', f'{args.version}', '-m', f'Release osbuild {args.version}'],
              None)
     elif repo == "osbuild-composer":
         res = step(f"Add and commit the release-relevant changes ({repo}.spec NEWS.md setup.py)",
@@ -415,8 +415,7 @@ def release_playbook(args, repo, api):
             run_command(['git', 'add', 'docs/news'])
             run_command(['git', 'commit', f'{repo}.spec', 'NEWS.md',
                         'docs/news/unreleased', f'docs/news/{args.version}', '-s',
-                        '-m', f'{args.version}', '-m', f'Release osbuild-composer {args.version}',
-                        '-m','[skip ci]'])
+                        '-m', f'{args.version}', '-m', f'Release osbuild-composer {args.version}'])
 
     step(f"Push all release changes to the remote '{args.remote}'",
          ['git', 'push', '--set-upstream', f'{args.remote}', f'release-{args.version}'], None)
