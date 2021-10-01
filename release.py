c858f797b92f4500dea338a18c4098eba71f670e
Simon Steinbeiss <simon.steinbeiss@redhat.com>
2021-10-01 09:09:13 +0200

    release-helper: Formatting

--- release.py
+++ release.py
@@ -397,14 +397,17 @@ def schedule_fedora_builds(repo):
     if os.path.isdir(repo) is False:
         run_command(['fedpkg','clone',repo])
     run_command(['cd',repo])
+
     for fedora in fedoras:
         msg_info(f"Scheduling build for Fedora {fedora}")
         run_command(['git','checkout',fedora])
         run_command(['git','pull'])
         res = run_command(['fedpkg','build'])
         print(res)
+
         if "completed successfully" in res:
             msg_ok(f"Build for {fedora} done.")
+
     msg_info(f"Check {url} for all {repo} builds.")
 
 
